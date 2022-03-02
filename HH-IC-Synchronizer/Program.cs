7aee586df3db8d7ca85a942ec4a0896b5b12ee0c
Oscar Davies <oscar.william.davies@outlook.com>
2022-03-02 07:31:43 +0000

    Improve ID Sync & WIP PO Sync

--- HH-IC-Synchronizer/Program.cs
+++ HH-IC-Synchronizer/Program.cs
@@ -5,35 +5,46 @@ using ICompleat;
 using ICompleat.Objects;
 using System.Linq;
 
-async Task Main()
+DateTime start = DateTime.Now;
+Hire_Hop_Interface.Interface.Connections.CookieConnection cookie = new Hire_Hop_Interface.Interface.Connections.CookieConnection();
+
+async Task Start()
 {
-    DateTime start = DateTime.Now; ;
     Console.WriteLine($"Sync Started At {start.ToString("dd/MM/yyyy HH:mm:ss")}");
 
-    Hire_Hop_Interface.Interface.Connections.CookieConnection cookie = new Hire_Hop_Interface.Interface.Connections.CookieConnection();
     await Hire_Hop_Interface.Interface.Authentication.Login(cookie, HH_IC_Synchronizer.Config.hh_email, HH_IC_Synchronizer.Config.hh_pword);
-
     Console.WriteLine("HH Login Complete");
+}
 
+Start().Wait();
+
+async Task SyncJobIds()
+{
     var r = await SearchResult.SearchForAll(new SearchResult.SearchOptions(), cookie);
-    var vals = r.results.OrderByDescending(x => x.jobDate).Select(x => new ICompleat.Objects.CustomFields.Field() { Code = x.id.Substring(1), Name = x.job_name.ToLower().Replace("&lt;", "").Replace("&gt;", "").Replace("&amp;", "") }).ToList();
-
-    for (int i = 0; i < vals.Count; i++)
-    {
-        int matches = vals.Take(i).Count(x => x.Name.ToLower().Split("%-")[0] == vals[i].Name.ToLower());
-        vals[i].Name = matches > 0 ? vals[i].Name + "%-" + matches : vals[i].Name;
-    }
+    var vals = r.results.OrderByDescending(x => x.jobDate).Select(x => new ICompleat.Objects.CustomFields.Field() { Code = x.id.Substring(1), Name = x.id.Substring(1) + " - " + x.job_name.ToLower().Replace("&lt;", "").Replace("&gt;", "").Replace("&amp;", "") }).ToList();
 
     Console.WriteLine($"Fetched {vals.Count} Jobs From HH");
 
     var fields = await ICompleat.Objects.CustomFields.GetCustomFieldsAsync();
     await fields[0].ReplaceValues(vals);
 
-    DateTime end = DateTime.Now;
-    TimeSpan dur = end - start;
-    Console.WriteLine($"Sync Finished At {end.ToString("dd/MM/yyyy HH:mm:ss")} Taking {dur.TotalSeconds} Seconds");
+    Console.WriteLine($"Pushed Custom Fields");
 }
 
-var t = Main();
+async Task SyncPOs()
+{
+    var r = await ICompleat.Objects.Transaction.GetTransactionsUntillAllAsync();
 
-t.Wait();
+    Console.WriteLine($"Fetched {r.Length} Transactions From IC");
+
+    var POs = r.Where(x => x.IsOrder);
+    var Invoicess = r.Where(x => x.IsInvoice);
+}
+
+var t = new Task[] { SyncJobIds(), SyncPOs() };
+
+Task.WaitAll(t);
+
+DateTime end = DateTime.Now;
+TimeSpan dur = end - start;
+Console.WriteLine($"Sync Finished At {end.ToString("dd/MM/yyyy HH:mm:ss")} Taking {dur.TotalSeconds} Seconds");
