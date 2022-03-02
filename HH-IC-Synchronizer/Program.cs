2d7fdd10a0ba6408b4dbe6e09edc8218e6dc5161
Oscar Davies <oscar.william.davies@outlook.com>
2022-03-02 10:28:57 +0000

    Sync Missing Contacts to HH

--- HH-IC-Synchronizer/Program.cs
+++ HH-IC-Synchronizer/Program.cs
@@ -31,10 +31,34 @@ async Task SyncJobIds()
     Console.WriteLine($"Pushed Custom Fields");
 }
 
+bool SupplierNameMatch(string s1, string s2)
+{
+    if (s1 == null || s2 == null || s1.Length == 0 || s2.Length == 0) return false;
+    s1 = s1.Replace("  ", " ");
+    s2 = s2.Replace("  ", " ");
+    return s1.Equals(s2, StringComparison.InvariantCultureIgnoreCase) || ((s1.StartsWith(s2) || s2.StartsWith(s1)) && Math.Abs(s1.Length - s2.Length) <= 5);
+}
+
+void LogUnmatchableSupplierNames(IEnumerable<Transaction> POsWithoutSyncedSupplier, IEnumerable<Supplier> suppliersSet)
+{
+    int i = 0;
+    List<Transaction> unmatchableTx = new List<Transaction>();
+    foreach (var tx in suppliersSet)
+    {
+        if (tx == null)
+        {
+            unmatchableTx.Add(POsWithoutSyncedSupplier.ElementAt(i));
+        }
+        i++;
+    }
+    var unmatchedSupplierNames = unmatchableTx.Select(x => x.SupplierName);
+}
+
 async Task SyncPOs()
 {
     var r = await ICompleat.Objects.Transaction.GetTransactionsUntillAllAsync();
     var c = await Hire_Hop_Interface.Objects.Contact.SearchForAll(cookie);
+    var s = await ICompleat.Objects.Supplier.GetSuppliersUntillAllAsync();
 
     Console.WriteLine($"Fetched {r.Length} Transactions From IC");
 
@@ -42,12 +66,30 @@ async Task SyncPOs()
     var Invoicess = r.Where(x => x.IsInvoice);
 
     var POsWithoutSyncedSupplier = POs.Where(x => !c.results.Any(
-        y => y.Company.Equals(x.SupplierName, StringComparison.InvariantCultureIgnoreCase) ||
-            y.Name.Equals(x.SupplierName, StringComparison.InvariantCultureIgnoreCase)
+        y => SupplierNameMatch(y.Company, x.SupplierName) ||
+            SupplierNameMatch(y.Name, x.SupplierName)
         ));
+
+    try
+    {
+        var suppliersSet = POsWithoutSyncedSupplier.Select(x => s.FirstOrDefault(y => SupplierNameMatch(y.Name, x.SupplierName)));
+
+        //LogUnmatchableSupplierNames(POsWithoutSyncedSupplier, suppliersSet);
+
+        var suppliersToSync = suppliersSet.Where(x => x != null).DistinctBy(x => x.Code).ToArray();
+
+        var supSyncs = suppliersToSync.Select(x => Hire_Hop_Interface.Objects.Contact.CreateNew(cookie, x.Name, $"{x.AddressLine1}\n{x.AddressLine2}\n{x.StateOrCounty}\n{x.PostcodeOrZip}\n{x.Country}", x.Telephone, x.Email)).ToArray();
+        Task.WaitAll(supSyncs);
+
+        Console.WriteLine($"Synced {supSyncs.Length} Contacts To HH");
+    }
+    catch (Exception e)
+    {
+        e = e;
+    }
 }
 
-var t = new Task[] { /*SyncJobIds(),*/ SyncPOs() };
+var t = new Task[] { SyncJobIds(), SyncPOs() };
 
 Task.WaitAll(t);
 
