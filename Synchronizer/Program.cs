155f59eb60c3344424ef88b3cc54c810d147ef60
Oscar Davies <oscar.william.davies@outlook.com>
2022-05-04 07:52:24 +0100

    Move ID & PO Sync Script Into One

---
+++ Synchronizer/Program.cs
@@ -0,0 +1,31 @@
+﻿DateTime start = DateTime.Now;
+Console.WriteLine($"Sync Started At {start.ToString("dd/MM/yyyy HH:mm:ss")}");
+
+Hire_Hop_Interface.Interface.Connections.CookieConnection cookie = new Hire_Hop_Interface.Interface.Connections.CookieConnection();
+
+async Task Main()
+{
+    await Hire_Hop_Interface.Interface.Authentication.Login(cookie, Synchronizer.Auth.PMY.hh_email, Synchronizer.Auth.PMY.hh_pword);
+    Console.WriteLine("HH Login Complete");
+
+    if (!await Hire_Hop_Interface.Interface.Authentication.ToggleAdmin(cookie))
+    {
+        await Hire_Hop_Interface.Interface.Authentication.ToggleAdmin(cookie);
+    }
+
+    Console.WriteLine("Syncing HH IDs To IC");
+    await Synchronizer.IDSync.SyncJobIds(cookie);
+
+    //Sync POs only every 1st 15 Minute Run
+    if (start.Minute / 15.0f < 1)
+    {
+        Console.WriteLine("Syncing IC POs To HH");
+        await Synchronizer.POSync.SyncPOs(cookie, Synchronizer.Auth.PMY, Synchronizer.Auth.ETHL);
+    }
+}
+
+Main().Wait();
+
+DateTime end = DateTime.Now;
+TimeSpan dur = end - start;
+Console.WriteLine($"Sync Finished At {end.ToString("dd/MM/yyyy HH:mm:ss")} Taking {dur.TotalSeconds} Seconds");
