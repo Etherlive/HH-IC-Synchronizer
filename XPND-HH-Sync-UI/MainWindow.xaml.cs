ed621e50741c736e8011781cf9beab688ab09c3e
Oscar Davies <oscar.william.davies@outlook.com>
2022-05-20 17:53:35 +0100

    Working Login Prompt

--- XPND-HH-Sync-UI/MainWindow.xaml.cs
+++ XPND-HH-Sync-UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hire_Hop_Interface.Interface;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using XPND_HH_Sync;
 
 namespace XPND_HH_Sync_UI
 {
@@ -22,10 +24,36 @@ namespace XPND_HH_Sync_UI
     public partial class MainWindow : Window
     {
         private string f_name = "./export.csv";
+        Hire_Hop_Interface.Interface.Connections.CookieConnection cookie = new Hire_Hop_Interface.Interface.Connections.CookieConnection();
+        Auth auth = XPND_HH_Sync.Auth.PMY;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            login_failed.Visibility = Visibility.Hidden;
+            if (auth.LoadFromFile())
+            {
+                hh_email.Text = auth.hh_email;
+                hh_password.Text = auth.hh_pword;
+
+                remeber_password.IsChecked = auth.hh_pword.Length > 0;
+            }
+        }
+        private async void hh_login_Click(object sender, RoutedEventArgs e)
+        {
+            string email = hh_email.Text, password = hh_password.Text;
+            bool loggedin = await Authentication.Login(cookie, email, password);
+            if (loggedin)
+            {
+                auth.hh_email = email;
+                auth.hh_pword = password;
+                auth.SaveDetails(remeberPassword: remeber_password.IsChecked.Value);
+            }
+            else
+            {
+                login_failed.Visibility = Visibility.Visible;
+            }
         }
 
         private void expend_file_select_Click(object sender, RoutedEventArgs e)
